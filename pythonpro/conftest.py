456680d2510630063d970391dd433f46789e50c5
renzon <renzon@gmail.com>
2017-10-28 16:05:23 -0200

    Added initial pytest infra to test template content
    #64
    
    Exposed Django TestCase as pytest fixtures

---
+++ pythonpro/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+from django.test import TestCase
+
+
+@pytest.fixture()
+def dj_test_case():
+    '''Fixture to give access to Django's TestCase object'''
+    return TestCase()
+
+
+@pytest.fixture()
+def dj_assert_contains(dj_test_case: TestCase):
+    '''Fixture to expose Django's TestCase assertContains as a function'''
+    return dj_test_case.assertContains
