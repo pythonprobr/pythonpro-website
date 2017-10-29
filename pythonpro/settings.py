0d80986dc266c5757f772222f620559ed03aedf4
renzon <renzon@gmail.com>
2017-10-29 21:19:04 -0200

    Added form and errors tests
    #64

--- pythonpro/settings.py
+++ pythonpro/settings.py
@@ -101,9 +101,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
