1cb4c93f1f82e61b4439d198503d6507a75e1a57
renzon <renzon@gmail.com>
2017-10-31 12:26:42 -0200

    Eliminated leakage of leads through error messages
    close #64

--- pythonpro/leads/views.py
+++ pythonpro/leads/views.py
@@ -1,5 +1,8 @@
-from django.http.response import HttpResponse
-from django.urls import reverse_lazy
+from django.core.exceptions import ValidationError
+from django.forms.fields import EmailField
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.urls.base import reverse_lazy
 from django.views.generic.edit import CreateView
 
 from pythonpro.leads.models import Lead
@@ -9,4 +12,17 @@ def subscribed(request):
     return HttpResponse(b'Ok')
 
 
-new = CreateView.as_view(model=Lead, fields='name email'.split(), success_url=reverse_lazy('leads:subscribed'))
+_create_lead_view = CreateView.as_view(
+    model=Lead, fields='name email'.split(), success_url=reverse_lazy('leads:subscribed'))
+
+
+def new(request):
+    """Creates a new Lead if the email is not already on database"""
+    email_field = EmailField()
+    try:
+        if Lead.objects.filter(email__exact=email_field.clean(request.POST['email'])).exists():
+            return HttpResponseRedirect(reverse('leads:subscribed'))
+    except ValidationError:
+        pass
+
+    return _create_lead_view(request)
