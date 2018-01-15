69a192659c958365960d65c7888ed269cc99adc2
renzon <renzon@gmail.com>
2018-01-15 16:46:34 -0200

    Home ok with MailChimp integration
    close #6

--- pythonpro/core/tests/test_view_home.py
+++ pythonpro/core/tests/test_view_home.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import pytest
 
 
@@ -17,11 +19,26 @@ def test_home_template(dj_assert_template_used):
 @pytest.mark.parametrize(
     'form_content',
     [
-        '<form action="/interessados/" method="post"',
-        '<input type="text" name="name"',
-        '<input type="email" name="email"',
+        '<form action="https://python.us17.list-manage.com/subscribe/post?u=ff7c56095f83f9c254edd749e&amp;id'
+        '=e3072e0df0"',
+        'method="post"',
+        '<input type="text" name="FNAME"',
+        '<input type="email" name="EMAIL"',
         '<button type="submit"'
     ]
 )
 def test_lead_form(form_content, dj_assert_contains, home_resp):
     dj_assert_contains(home_resp, form_content)
+
+
+@pytest.mark.parametrize(
+    'name,value',
+    [
+        ('day', date.today().strftime('%d')),
+        ('month', date.today().strftime('%m')),
+        ('year', date.today().strftime('%Y')),
+    ]
+)
+def test_subscription_today_date(name, value, dj_assert_contains, home_resp):
+    value = f'<input type="hidden" name="SUBSCRIP[{name}]" value="{value}"'
+    dj_assert_contains(home_resp, value)
