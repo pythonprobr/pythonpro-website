139389fbf47f6b5e2d2d369b593a20239410d29f
renzon <renzon@gmail.com>
2018-01-17 14:01:35 -0200

    Added Video Model
    related #149

--- pythonpro/promos/models.py
+++ pythonpro/promos/models.py
@@ -1,2 +1,7 @@
+from django.db import models
 
-# Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    vimeo_id = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
