4ab5dc9978a2e38fb5a83dffa32572559f11a1d0
renzon <renzon@gmail.com>
2018-01-17 14:17:53 -0200

    Added Video to Admin
    close #149

--- pythonpro/promos/admin.py
+++ pythonpro/promos/admin.py
@@ -1,2 +1,10 @@
+from django.contrib import admin
 
-# Register your models here.
+from pythonpro.promos.models import Video
+
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+admin.site.register(Video, VideoAdmin)
