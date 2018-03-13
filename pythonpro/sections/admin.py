4c1e03ebdf2f15324f3d0d124830fc1debce08e5
renzon <renzon@gmail.com>
2018-03-13 17:13:56 -0300

    Implemented basic admin for Section
    related to #45

--- pythonpro/sections/admin.py
+++ pythonpro/sections/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from pythonpro.sections.models import Section
+
+
+class SectionAdmin(admin.ModelAdmin):
+    list_display = 'title slug _module_slug'.split()
+
+
+admin.site.register(Section, SectionAdmin)
--- pythonpro/sections/models.py
+++ pythonpro/sections/models.py
@@ -1,8 +1,13 @@
 from django.db import models
 
+from pythonpro.modules import ALL_MODULES
+
 
 class Section(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
     slug = models.SlugField(unique=True)
-    _module_slug = models.SlugField()
+    _module_slug = models.SlugField(choices=((m.slug, m.title) for m in ALL_MODULES.values()))
+
+    class Meta:
+        ordering = ['_module_slug', 'title']
