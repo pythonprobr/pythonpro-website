78cc72bf1b2389eaba6b868f63a2633de79d28c0
renzon <renzon@gmail.com>
2018-03-13 16:49:51 -0300

    Implemented model Sectionˆ
    related to #45

--- pythonpro/sections/tests.py
+++ pythonpro/sections/tests.py
@@ -2,13 +2,27 @@ import pytest
 from django.urls import reverse
 from model_mommy import mommy
 
+from pythonpro.django_assertions import dj_assert_contains
+from pythonpro.modules import PYTHON_BIRDS
+from pythonpro.sections.models import Section
+
 
 @pytest.fixture
-def resp(client, django_user_model):
+def section():
+    return mommy.make(Section, slug='procedural', _module_slug=PYTHON_BIRDS.slug)
+
+
+@pytest.fixture
+def resp(client, django_user_model, section):
     user = mommy.make(django_user_model)
     client.force_login(user)
-    return client.get(reverse('sections:detail', kwargs={'slug': 'procedural'}))
+    return client.get(reverse('sections:detail', kwargs={'slug': section.slug}))
 
 
 def test_status_code(resp):
     assert resp.status_code == 200
+
+
+@pytest.mark.parametrize('property_name', ['title', 'description'])
+def test_title(resp, section, property_name):
+    dj_assert_contains(resp, getattr(section, property_name))
