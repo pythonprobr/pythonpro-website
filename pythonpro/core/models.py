1b6854c13881a0acb5e234ff33d0a7211dafc324
renzon <renzon@gmail.com>
2018-05-30 18:28:27 -0300

    Implemented login with email
    close #405

--- pythonpro/core/models.py
+++ pythonpro/core/models.py
@@ -18,16 +18,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(
         _('username'),
         max_length=150,
-        unique=True,
         help_text=_('Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.'),
         validators=[username_validator],
-        error_messages={
-            'unique': _("A user with that username already exists."),
-        },
+
     )
     first_name = models.CharField(_('first name'), max_length=30, blank=True)
     last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    email = models.EmailField(_('email address'), blank=True)
+    email = models.EmailField(_('email address'), unique=True,  error_messages={
+            'unique': _("A user with that username already exists."),
+        })
     is_staff = models.BooleanField(
         _('staff status'),
         default=False,
@@ -46,8 +45,8 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = _('user')
