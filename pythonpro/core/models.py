354ba480056066eb25b46278fde026db6c63442d
renzon <renzon@gmail.com>
2018-06-01 10:21:53 -0300

    Fixed createsuperadmin command
    close #415

--- pythonpro/core/models.py
+++ pythonpro/core/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.base_user import AbstractBaseUser
-from django.contrib.auth.models import PermissionsMixin, UserManager, send_mail
+from django.contrib.auth.models import PermissionsMixin, send_mail
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.utils import timezone
+
+from pythonpro.core.managers import UserManager
 
 
 class User(AbstractBaseUser, PermissionsMixin):
