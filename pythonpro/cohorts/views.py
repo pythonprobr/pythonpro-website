9f42e91318d806981af181969e0fcbfa4b94bac8
renzon <renzon@gmail.com>
2018-06-03 23:22:57 -0300

    Implemented Cohort page only available for logged users
    close #423

--- pythonpro/cohorts/views.py
+++ pythonpro/cohorts/views.py
@@ -1,8 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
 from pythonpro.cohorts.models import Cohort
 
 
+@login_required
 def detail(request, slug):
     return render(request, 'cohorts/cohort_detail.html', {'cohort': Cohort.objects.get(slug=slug)})
