c26725eb46685bde84f914b786ec009d87fbac16
renzon <renzon@gmail.com>
2018-06-06 10:34:48 -0300

    Embedded YouTube Teck Talks
    close #445

--- pythonpro/core/urls.py
+++ pythonpro/core/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'core'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tech-talks', views.teck_talks, name='tech_talks'),
     path('obrigado', views.thanks, name='thanks'),
     path('perfil', views.profile, name='profile'),
     path('perfil/nome', views.profile_name, name='profile_name'),
