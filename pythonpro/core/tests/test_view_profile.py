c2c11bc78aab07eed4c0972780dec4fa9b732420
alisonamerico <alison.americo@gmail.com>
2018-06-26 00:51:01 -0300

    Added password validation on user profile screen

--- pythonpro/core/tests/test_view_profile.py
+++ pythonpro/core/tests/test_view_profile.py
@@ -47,3 +47,5 @@ def test_edit_email_link(resp_with_user):
     dj_assert_contains(resp_with_user, reverse('core:profile_email'))
 
 
+def test_edit_password_link(resp_with_user):
+    dj_assert_contains(resp_with_user, reverse('core:profile_password'))
