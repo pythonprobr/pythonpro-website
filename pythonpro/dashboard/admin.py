6dffd61add4ed460a2a486b865a0035dcd87ab14
renzon <renzon@gmail.com>
2019-06-24 12:35:57 -0300

    Changed client LP to conform with Pipenv section addition
    close #1265

--- pythonpro/dashboard/admin.py
+++ pythonpro/dashboard/admin.py
@@ -6,5 +6,4 @@ from pythonpro.dashboard.models import TopicInteraction
 @admin.register(TopicInteraction)
 class TopicInteractionAdmin(admin.ModelAdmin):
     list_display = 'creation user topic topic_duration total_watched_time max_watched_time'.split()
-    list_filter = ('topic', 'user')
     ordering = ('-creation',)
