ec5749e7cfb686ca69f4e332a7d8ffc72a5b801a
renzon <renzon@gmail.com>
2019-06-26 16:20:17 -0300

    Added Google Tag Manager
    close #1273
    
    Removed Pixels from Facebook and Hotjar since they'll be managed by GTM

--- pythonpro/core/context_processors.py
+++ pythonpro/core/context_processors.py
@@ -6,7 +6,6 @@ def global_settings(request):
     dct = {
         'DEFAULT_FROM_EMAIL': settings.DEFAULT_FROM_EMAIL,
         'DISCOURSE_BASE_URL': settings.DISCOURSE_BASE_URL,
-        'FACEBOOK_PIXEL_ID': settings.FACEBOOK_PIXEL_ID,
         'GOOGLE_TAG_MANAGER_ID': settings.GOOGLE_TAG_MANAGER_ID,
     }
     return dct
