cfd51e92b1b82fd2e38b97a8b45ad275b07d8ee7
renzon <renzon@gmail.com>
2019-06-26 17:48:17 -0300

    Added Mailchimp tracking for user who accessed checkout
    close #1277

--- pythonpro/payments/tests/test_landing_pages.py
+++ pythonpro/payments/tests/test_landing_pages.py
@@ -11,3 +11,14 @@ def client_lp_resp(client_with_lead, mocker, logged_user):
 
 def test_non_logged_status_code(client_lp_resp):
     assert client_lp_resp.status_code == 200
+
+
+@pytest.fixture
+def client_checkout_click_resp(client_with_lead, mocker, logged_user):
+    tag_as = mocker.patch('pythonpro.payments.views.tag_as')
+    yield client_with_lead.post(reverse('payments:client_checkout'), secure=True)
+    tag_as.assert_called_once_with(logged_user.email, 'client-checkout')
+
+
+def test_checkout_click(client_checkout_click_resp):
+    assert client_checkout_click_resp.status_code == 200
--- pythonpro/payments/urls.py
+++ pythonpro/payments/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('opcoes/', views.options, name='options'),
     path('obrigado/', views.thanks, name='thanks'),
     path('curso-completo-de-python', views.member_landing_page, name='member_landing_page'),
+    path('client-checkout', views.client_checkout, name='client_checkout'),
     path('lista-de-espera', views.waiting_list_ty, name='waiting_list_ty'),
     path('pytools/obrigado/', views.pytools_thanks, name='pytools_thanks'),
     path('pytools/captura/', views.pytools_capture, name='pytools_capture'),
