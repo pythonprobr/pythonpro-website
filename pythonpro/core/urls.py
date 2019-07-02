92ff29e1c1040be12bbad70d0a9c1c4fa74480c8
Moacir Moda <moa.moda@gmail.com>
2019-07-02 13:13:37 -0300

    letting lead user define password in TY page. resolves #1297

--- pythonpro/core/urls.py
+++ pythonpro/core/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('podcast', views.podcast, name='podcast'),
     path('curso-de-python-gratis', views.lead_landing, name='lead_landing'),
     path('cadastro-python-birds', views.lead_form, name='lead_form'),
+    path('definir-senha', views.lead_change_password, name='lead_change_password'),
     path('obrigado', views.thanks, name='thanks'),
     path('perfil', views.profile, name='profile'),
     path('perfil/nome', views.profile_name, name='profile_name'),
