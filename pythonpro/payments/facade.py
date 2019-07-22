fa4535e6000b753be02248d75aac11a25ccf458e
renzon <renzon@gmail.com>
2019-07-22 20:20:50 -0300

    Implemented promotion on seventh week
    close #1317

--- pythonpro/payments/facade.py
+++ pythonpro/payments/facade.py
@@ -1,5 +1,10 @@
+from datetime import datetime
+from typing import Tuple
+
 import pagarme as _pagarme
+from dateutil.relativedelta import MO, TH, relativedelta
 from django.conf import settings
+from django.utils.timezone import now
 
 _pagarme.authentication_key(settings.PAGARME_API_KEY)
 PYTOOLS_PRICE = 9999
@@ -33,3 +38,26 @@ def extract_transaction(notification: dict, raw_post: str, expected_signature):
     if notification['object'] != 'transaction' or notification['current_status'] != 'paid':
         raise PagarmeNotPaidTransaction()
     return _pagarme.transaction.find_by_id(notification['transaction[id]'])
+
+
+def calculate_pytools_promotion_interval() -> Tuple[datetime, datetime]:
+    """
+    calculate promotion interval for this week based on time. Promotion will begin on monday and stop on Thursday
+    :return:
+    """
+    now_dt = now()
+    this_week_monday = now_dt + relativedelta(weekday=MO(-1), hour=0, minute=0, second=0)
+    this_week_thursday = this_week_monday + relativedelta(weekday=TH, hour=23, minute=59, second=59)
+    return this_week_monday, this_week_thursday
+
+
+def is_on_pytools_promotion_season(creation: datetime) -> bool:
+    """
+    Calculate if is period of promotion which is 7 weeks after creation
+    :param creation: datetime of creation
+    :return: boolean indication if its os promotion period or not
+    """
+    promotion_begin, _ = calculate_pytools_promotion_interval()
+    creation_begin = promotion_begin + relativedelta(weekday=MO(-8))
+    creation_end = creation_begin + relativedelta(days=6, hour=23, minute=59, second=59)
+    return creation_begin <= creation <= creation_end
---
+++ pythonpro/payments/tests/test_pytools_promo.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+import pytest
+import pytz
+from freezegun import freeze_time
+
+from pythonpro.payments.facade import calculate_pytools_promotion_interval, is_on_pytools_promotion_season
+
+
+@pytest.mark.parametrize(
+    'now', ['2019-07-{}'.format(day) for day in range(22, 29)]
+)
+def test_promotion_interval(now):
+    with freeze_time(now):
+        begin, end = datetime(2019, 7, 22, 0, 0, 0, tzinfo=pytz.utc), datetime(2019, 7, 25, 23, 59, 59, tzinfo=pytz.utc)
+        assert (begin, end) == calculate_pytools_promotion_interval()
+
+
+@pytest.mark.parametrize(
+    'creation', [datetime(2019, 6, day, tzinfo=pytz.utc) for day in range(3, 10)]  # 7 weeks before now
+)
+def test_in_promotion_period(creation: datetime):
+    with freeze_time('2019-07-22'):
+        assert is_on_pytools_promotion_season(creation)
+
+
+@pytest.mark.parametrize(
+    'creation', [datetime(2019, 6, day, tzinfo=pytz.utc) for day in range(1, 2)]  # 8 weeks before now
+)
+def test_before_promotion_period(creation: datetime):
+    with freeze_time('2019-07-22'):
+        assert not is_on_pytools_promotion_season(creation)
+
+
+@pytest.mark.parametrize(
+    'creation', [datetime(2019, 6, day, tzinfo=pytz.utc) for day in range(10, 20)]  # 6 weeks before now
+)
+def test_after_promotion_period(creation: datetime):
+    with freeze_time('2019-07-22'):
+        assert not is_on_pytools_promotion_season(creation)
