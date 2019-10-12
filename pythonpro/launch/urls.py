ce0174e67bf65786a7d05bae1f3922b1181116fc
Moacir Moda <moa.moda@gmail.com>
2019-10-12 13:37:11 -0300

    changing URL names

--- pythonpro/launch/urls.py
+++ pythonpro/launch/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = 'launch'
 urlpatterns = [
-    path('semana-do-programador-profissional', views.landing_page, name='landing_page'),
+    path('semana-do-programador-profissional/inscricao', views.landing_page, name='landing_page'),
     path('semana-do-programador-profissional/form', views.lead_form, name='lead_form'),
     path('semana-do-programador-profissional/obrigado', views.ty, name='ty'),
 ]
