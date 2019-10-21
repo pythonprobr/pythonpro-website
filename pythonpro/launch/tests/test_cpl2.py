ce8a3913859b205988670fa3afb3006feb0aa80d
Moacir Moda <moa.moda@gmail.com>
2019-10-21 10:31:40 -0300

    creating states to identify launch phases and adapting the page behaviors

--- pythonpro/launch/tests/test_cpl2.py
+++ pythonpro/launch/tests/test_cpl2.py
@@ -10,7 +10,12 @@ def tag_as_mock(mocker):
 
 
 @pytest.fixture
-def resp(client, tag_as_mock):
+def launch_status_as_mock(mocker):
+    return mocker.patch('pythonpro.launch.views._get_launch_status', return_value=4)
+
+
+@pytest.fixture
+def resp(client, tag_as_mock, launch_status_as_mock):
     return client.get(reverse('launch:cpl2'), secure=True)
 
 
