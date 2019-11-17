f80896517b966a217aaa1cd76c3e3da2bd52b0c2
Moacir Moda <moa.moda@gmail.com>
2019-11-17 12:08:18 -0300

    creating admin for PageView model

--- pythonpro/analytics/admin.py
+++ pythonpro/analytics/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from pythonpro.analytics.models import PageView
+
+
+@admin.register(PageView)
+class PageViewAdmin(admin.ModelAdmin):
+    list_display = ['id', 'session', 'created']
