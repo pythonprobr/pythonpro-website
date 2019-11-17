4f6807fa88f62ad60211a1c7a393cb67a94d6bb2
Moacir Moda <moa.moda@gmail.com>
2019-11-17 12:10:49 -0300

    implementing facade create_pageview

--- pythonpro/analytics/facade.py
+++ pythonpro/analytics/facade.py
@@ -1,4 +1,6 @@
-from pythonpro.analytics.models import UserSession
+import json
+
+from pythonpro.analytics.models import UserSession, PageView
 
 
 def get_or_create_session(request):
@@ -13,3 +15,19 @@ def get_or_create_session(request):
     session = UserSession.objects.create()
     request.session['analytics_session'] = str(session.uuid)
     return request
+
+
+def _get_serialized_meta(meta):
+    class ComplexEncoder(json.JSONEncoder):
+        def default(self, obj):
+            try:
+                return json.JSONEncoder.default(self, obj)
+            except TypeError:
+                return None
+
+    return json.loads(json.dumps(meta, cls=ComplexEncoder))
+
+
+def create_pageview(user_session, request):
+    PageView.objects.create(session=user_session,
+                            meta=_get_serialized_meta(request.META))
--- pythonpro/analytics/tests/test_middleware.py
+++ pythonpro/analytics/tests/test_middleware.py
@@ -1,10 +1,19 @@
+import pytest
+
+
 def test_should_assert_that_middleware_exists():
     from pythonpro.analytics.middleware import AnalyticsMiddleware
 
 
-# def test_should_execute_middleware_when_request_is_made(client, mocker):
-#     mocked = mocker.patch(
-#         'pythonpro.analytics.middleware.AnalyticsMiddleware.process_response')
+@pytest.mark.django_db
+def test_should_execute_get_or_create_session(client, mocked_request, mocker):
+    mocked = mocker.patch(
+        'pythonpro.analytics.middleware.get_or_create_session',
+        return_value=mocked_request)
 
-#     client.get('/')
-#     assert mocked.called
+    client.get('/', follow=True)
+    assert mocked.called
+
+
+def test_should_execute_create_pagewview():
+    assert False
