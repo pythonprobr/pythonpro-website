807056497f7f2fdc123a8b64e2183e73e4704ae4
Moacir Moda <moa.moda@gmail.com>
2019-11-17 12:08:02 -0300

    adding PageView model to analytics app

--- pythonpro/analytics/models.py
+++ pythonpro/analytics/models.py
@@ -1,18 +1,36 @@
 import uuid
 
 from django.utils import timezone
-
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 
-class UserSession(models.Model):
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+
+    created = models.DateTimeField('Criado em', default=timezone.now)
+    updated = models.DateTimeField('Alterado em', auto_now=True)
+
+
+class UserSession(BaseModel):
     class Meta:
         verbose_name = 'sessão'
         verbose_name_plural = 'sessões'
 
-    created = models.DateTimeField('Criado em', default=timezone.now)
-    updated = models.DateTimeField('Alterado em', auto_now=True)
     uuid = models.UUIDField(default=uuid.uuid4, editable=False)
 
     def __str__(self):
         return str(self.uuid)
+
+
+class PageView(BaseModel):
+    class Meta:
+        verbose_name = 'page view'
+        verbose_name_plural = 'page views'
+
+    session = models.ForeignKey(UserSession,
+                                verbose_name='sessão',
+                                on_delete=models.SET_NULL,
+                                null=True)
+    meta = JSONField()
