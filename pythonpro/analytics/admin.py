0d378abc5e9d31a86b9cde8b4c668be587fc71f8
Moacir Moda <moa.moda@gmail.com>
2019-11-19 17:44:50 -0300

    improving analytics admin

--- pythonpro/analytics/admin.py
+++ pythonpro/analytics/admin.py
@@ -5,4 +5,22 @@ from pythonpro.analytics.models import PageView
 
 @admin.register(PageView)
 class PageViewAdmin(admin.ModelAdmin):
-    list_display = ['id', 'session', 'created']
+    def get_session_user(self, obj, *args, **kwargs):
+        return obj.session.user
+
+    def get_path(self, obj, *args, **kwargs):
+        return obj.meta.get('PATH_INFO')
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request, obj=None):
+        return False
+
+    get_session_user.short_description = 'Usuário'
+    get_path.short_description = 'Destino'
+    list_display = ['id', 'get_path', 'session', 'get_session_user', 'created']
+    list_filter = ['session__user']
