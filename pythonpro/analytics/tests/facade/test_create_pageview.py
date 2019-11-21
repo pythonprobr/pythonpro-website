d71c338cc7acdd0d63b45b5c4c1795cae17fa3bd
Moacir Moda <moa.moda@gmail.com>
2019-11-20 23:12:02 -0300

    adding secure=True in mocked requests and removing imports inside tests functions

--- pythonpro/analytics/tests/facade/test_create_pageview.py
+++ pythonpro/analytics/tests/facade/test_create_pageview.py
@@ -1,46 +1,36 @@
 import pytest
 
+from pythonpro.analytics.facade import create_pageview, _should_create_pageview
 from pythonpro.analytics.models import PageView
 
 
-def test_should_facade_exists():
-    from pythonpro.analytics import facade
-    assert facade.create_pageview
-
-
 @pytest.fixture
-def create_pageview(mocked_request_with_analytics):
-    from pythonpro.analytics.facade import create_pageview
-
-    return create_pageview(mocked_request_with_analytics)
-
-
-@pytest.fixture
-def create_pageview_2(mocked_request_with_analytics):
-    from pythonpro.analytics.facade import create_pageview
-
+def create_pageview_executed(mocked_request_with_analytics):
     return create_pageview(mocked_request_with_analytics)
 
 
 @pytest.mark.django_db
-def test_should_create_pageview_on_database(create_pageview):
+def test_should_create_pageview_on_database(create_pageview_executed):
     assert PageView.objects.all().count() == 1
 
 
 @pytest.mark.django_db
-def test_should_create_meta_field_as_dict(create_pageview):
+def test_should_create_meta_field_as_dict(create_pageview_executed):
     assert isinstance(PageView.objects.get().meta, dict)
 
 
+@pytest.fixture
+def create_pageview_executed_2(mocked_request_with_analytics):
+    return create_pageview(mocked_request_with_analytics)
+
+
 @pytest.mark.django_db
-def test_should_create_pageview_for_each_request(create_pageview,
-                                                 create_pageview_2):
+def test_should_create_pageview_for_each_request(create_pageview_executed,
+                                                 create_pageview_executed_2):
     assert PageView.objects.all().count() == 2
 
 
 def test_should_ignore_all_urls_that_starts_with_admin():
-    from pythonpro.analytics.facade import _should_create_pageview
-
     assert _should_create_pageview('/curso-gratis-de-python') is True
     assert _should_create_pageview('/admin/analytics/pageview') is False
     assert _should_create_pageview('/admin') is False
@@ -49,8 +39,6 @@ def test_should_ignore_all_urls_that_starts_with_admin():
 @pytest.mark.django_db
 def test_should_call_should_create_pageview_function(
         mocked_request_with_analytics, mocker):
-    from pythonpro.analytics.facade import create_pageview
-
     mocked = mocker.patch('pythonpro.analytics.facade._should_create_pageview',
                           return_value=True)
 
