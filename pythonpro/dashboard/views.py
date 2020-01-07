96e68e5528cbc6f0df90942cfb8c4eaa0151f7af
renzon <renzon@gmail.com>
2020-01-07 09:16:14 -0300

    Implemented topic aggregation
    close #1848

--- pythonpro/dashboard/views.py
+++ pythonpro/dashboard/views.py
@@ -1,18 +1,42 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Max, Sum
 from django.http import JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 
 from pythonpro.dashboard.facade import has_watched_any_topic
 from pythonpro.dashboard.forms import TopicInteractionForm
-from pythonpro.dashboard.models import TopicInteraction
 from pythonpro.domain import user_facade
+from pythonpro.modules.models import Topic
 
 
 @login_required
 def home(request):
-    interactions = TopicInteraction.objects.select_related('topic').filter(user=request.user).order_by('-creation')[:20]
-    return render(request, 'dashboard/home.html', {'interactions': interactions})
+    topics = list(
+        Topic.objects.filter(
+            topicinteraction__user=request.user
+        ).annotate(
+            last_interaction=Max('topicinteraction__creation')
+        ).annotate(
+            max_watched_time=Max('topicinteraction__max_watched_time')
+        ).annotate(
+            total_watched_time=Sum('topicinteraction__total_watched_time')
+        ).annotate(
+            interactions_count=Count('topicinteraction')
+        ).order_by('-last_interaction').select_related('chapter').select_related('chapter__section').select_related(
+            'chapter__section__module')[:20]
+    )
+
+    for topic in topics:
+        topic.calculated_module = topic.find_module()
+
+    return render(
+        request,
+        'dashboard/home.html',
+        {
+            'topics': topics,
+        }
+    )
 
 
 @login_required
