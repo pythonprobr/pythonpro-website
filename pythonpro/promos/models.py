085dc4ce7ffb8cdfc6a7656a9467e97cb8053573
renzon <renzon@gmail.com>
2020-01-27 14:01:55 -0300

    Created migration do drop Video from promos app
    part 1/2 of #1208

--- pythonpro/promos/models.py
+++ pythonpro/promos/models.py
@@ -1,11 +1,10 @@
-from django.db import models
-from django.urls import reverse
+class Video():
+    pass
 
-
-class Video(models.Model):
-    title = models.CharField(max_length=100)
-    vimeo_id = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100, unique=True)
-
-    def get_absolute_url(self):
-        return reverse('promos:video', kwargs={'slug': self.slug})
+# class Video(models.Model):
+#     title = models.CharField(max_length=100)
+#     vimeo_id = models.CharField(max_length=100)
+#     slug = models.SlugField(max_length=100, unique=True)
+#
+#     def get_absolute_url(self):
+#         return reverse('promos:video', kwargs={'slug': self.slug})
