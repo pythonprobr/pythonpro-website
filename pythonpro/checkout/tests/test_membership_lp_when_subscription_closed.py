71d7fb7d18787c5b02776a468baf9aeba3278d84
renzon <renzon@gmail.com>
2020-03-25 18:54:10 -0300

    Implemented membership landing page
    close #2213

---
+++ pythonpro/checkout/tests/test_membership_lp_when_subscription_closed.py
@@ -0,0 +1,50 @@
+from datetime import timedelta
+
+import pytest
+from django.urls import reverse
+
+from pythonpro.checkout import facade
+from pythonpro.django_assertions import dj_assert_contains
+
+
+@pytest.fixture
+def tag_as_mock(mocker):
+    return mocker.patch('pythonpro.domain.user_facade._email_marketing_facade.tag_as.delay')
+
+
+@pytest.fixture
+def resp(client, tag_as_mock, freezer, logged_user):
+    freezer.move_to(facade.launch_datetime_finish + timedelta(seconds=1))
+    client.force_login(logged_user)
+    return client.get(reverse('checkout:membership_lp'), secure=True)
+
+
+def test_tag_as_called(resp, logged_user, tag_as_mock):
+    tag_as_mock.assert_called_once_with(logged_user.email, logged_user.id, 'potential-member')
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_subscription_link_is_present(resp):
+    dj_assert_contains(resp, reverse('payments:waiting_list_ty'))
+
+
+@pytest.fixture
+def resp_no_user(client, freezer):
+    freezer.move_to(facade.launch_datetime_finish + timedelta(seconds=1))
+    resp = client.get(reverse('checkout:membership_lp'), secure=True)
+    return resp
+
+
+def test_no_user_status_code(resp_no_user):
+    assert resp_no_user.status_code == 200
+
+
+def test_no_user_waiting_list_link(resp_no_user):
+    dj_assert_contains(resp_no_user, reverse('payments:waiting_list_ty'))
+
+
+def test_no_tagging(resp_no_user, tag_as_mock):
+    assert tag_as_mock.call_count == 0
--- pythonpro/checkout/urls.py
+++ pythonpro/checkout/urls.py
@@ -6,4 +6,5 @@ app_name = 'checkout'
 urlpatterns = [
     path('curso-de-python-intermediario', views.pytools_lp, name='pytools_lp'),
     path('curso-de-python-intermediario-oto', views.pytools_oto_lp, name='pytools_oto_lp'),
+    path('inscricao', views.membership_lp, name='membership_lp'),
 ]
