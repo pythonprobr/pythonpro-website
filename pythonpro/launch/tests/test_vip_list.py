f16641749c2ec1d26c9f112db7efe9a8f07adfa0
Moacir Moda <moa.moda@gmail.com>
2020-04-07 17:49:17 -0300

    creating page to subscribe to vip list

---
+++ pythonpro/launch/tests/test_vip_list.py
@@ -0,0 +1,17 @@
+import pytest
+from django.urls import reverse
+
+from pythonpro.django_assertions import dj_assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('launch:vip_list'), secure=True)
+
+
+def test_status_code(resp):
+    assert 200 == resp.status_code
+
+
+def test_email_field_is_present(resp):
+    dj_assert_contains(resp, 'https://python.activehosted.com/f/embed.php?id=2')
--- pythonpro/launch/urls.py
+++ pythonpro/launch/urls.py
@@ -1,6 +1,8 @@
+from django.views.generic.base import TemplateView
 from django.urls import path
 
 from . import views
+
 
 app_name = 'launch'
 urlpatterns = [
@@ -12,4 +14,9 @@ urlpatterns = [
     path('semana-do-programador-profissional/aula-3', views.cpl3, name='cpl3'),
     path('OneSignalSDKWorker.js', views.onesignal_sdk_worker),
     path('OneSignalSDUpdaterKWorker.js', views.onesignal_sdk_worker),
+    path(
+        'semana-do-programador-profissional/lista-vip',
+        TemplateView.as_view(template_name='launch/vip-list.html'),
+        name='vip_list'
+    ),
 ]
