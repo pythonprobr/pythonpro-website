8422f069f05343dea9d8c6d16ecc39f7500bd821
Moacir Moda <moa.moda@gmail.com>
2020-04-08 12:12:39 -0300

    First version of launch blog of SPP
    close #2260

---
+++ pythonpro/launch/tests/test_cpl.py
@@ -0,0 +1,76 @@
+import pytest
+from django.urls import reverse
+
+from pythonpro.django_assertions import dj_assert_contains
+from pythonpro.launch.facade import (
+    LAUNCH_STATUS_CPL1,
+    LAUNCH_STATUS_CPL2,
+    LAUNCH_STATUS_CPL3,
+    LAUNCH_STATUS_CPL4
+)
+
+
+@pytest.fixture
+def tag_as_mock(mocker):
+    return mocker.patch('pythonpro.domain.user_facade._email_marketing_facade.tag_as.delay')
+
+
+@pytest.fixture
+def resp_on_cpl1_on_air(mocker, client, tag_as_mock):
+    mocker.patch('pythonpro.launch.facade.get_launch_status', return_value=LAUNCH_STATUS_CPL1)
+    mocker.patch('pythonpro.launch.views.get_launch_status', return_value=LAUNCH_STATUS_CPL1)
+    return client.get(reverse('launch:cpl1'), secure=True)
+
+
+def test_cpl_video_is_present(resp_on_cpl1_on_air):
+    dj_assert_contains(resp_on_cpl1_on_air, 'https://www.youtube.com/embed/')
+
+
+def test_facebook_comments_is_present(resp_on_cpl1_on_air):
+    dj_assert_contains(resp_on_cpl1_on_air, f'www.facebook.com/v4.0/plugins/comments.php')
+
+
+def test_should_return_cpl1_on_air_in_navbar(resp_on_cpl1_on_air):
+    dj_assert_contains(resp_on_cpl1_on_air, reverse('launch:cpl1'))
+
+
+@pytest.fixture
+def resp_on_cpl2_on_air(mocker, client, tag_as_mock):
+    mocker.patch('pythonpro.launch.facade.get_launch_status', return_value=LAUNCH_STATUS_CPL2)
+    mocker.patch('pythonpro.launch.views.get_launch_status', return_value=LAUNCH_STATUS_CPL2)
+    return client.get(reverse('launch:cpl2'), secure=True)
+
+
+def test_should_return_cpl2_on_air_in_navbar(resp_on_cpl2_on_air):
+    dj_assert_contains(resp_on_cpl2_on_air, reverse('launch:cpl2'))
+
+
+@pytest.fixture
+def resp_on_cpl3_on_air(mocker, client, tag_as_mock):
+    mocker.patch('pythonpro.launch.facade.get_launch_status', return_value=LAUNCH_STATUS_CPL3)
+    mocker.patch('pythonpro.launch.views.get_launch_status', return_value=LAUNCH_STATUS_CPL3)
+    return client.get(reverse('launch:cpl3'), secure=True)
+
+
+def test_should_return_cpl3_on_air_in_navbar(resp_on_cpl3_on_air):
+    dj_assert_contains(resp_on_cpl3_on_air, reverse('launch:cpl3'))
+
+
+@pytest.fixture
+def resp_on_cpl4_on_air(mocker, client, tag_as_mock):
+    mocker.patch('pythonpro.launch.facade.get_launch_status', return_value=LAUNCH_STATUS_CPL4)
+    mocker.patch('pythonpro.launch.views.get_launch_status', return_value=LAUNCH_STATUS_CPL4)
+    return client.get(reverse('launch:cpl4'), secure=True)
+
+
+def test_should_return_cpl4_on_air_in_navbar(resp_on_cpl4_on_air):
+    dj_assert_contains(resp_on_cpl4_on_air, reverse('launch:cpl4'))
+
+
+@pytest.fixture
+def resp_cpl_with_debug(mocker, client, tag_as_mock):
+    return client.get(reverse('launch:cpl1') + '?debug=1', secure=True)
+
+
+def test_should_return_cpl_when_debug_is_true(resp_cpl_with_debug):
+    assert resp_cpl_with_debug.status_code == 200
--- pythonpro/launch/tests/test_launch_status.py
+++ pythonpro/launch/tests/test_launch_status.py
@@ -1,161 +1,107 @@
 from datetime import datetime
 
-import pytest
-
-from django.urls import reverse
 from django.utils import timezone
 
-from pythonpro.django_assertions import dj_assert_contains
-from pythonpro.launch.views import (
-    _get_launch_status,
+from pythonpro.launch.facade import (
+    get_launch_status,
+    get_opened_cpls,
     LAUNCH_STATUS_CPL1,
     LAUNCH_STATUS_CPL2,
     LAUNCH_STATUS_CPL3,
+    LAUNCH_STATUS_CPL4,
     LAUNCH_STATUS_PPL,
     LAUNCH_STATUS_OPEN_CART,
+    LAUNCH_STATUS_CLOSED
 )
 
 
 def test_should_return_launch_status_ppl_before_monday(mocker):
-    date = timezone.make_aware(datetime(2019, 10, 27))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_PPL
+    fake_today = timezone.make_aware(datetime(2020, 4, 12, 23, 59, 59))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_PPL
 
 
-def test_should_return_launch_status_cpl1_on_monday(mocker):
-    date = timezone.make_aware(datetime(2019, 10, 28))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_CPL1
+def test_should_return_launch_status_cpl1_on_mon_early(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 13))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_CPL1
 
 
-def test_should_return_launch_status_cpl1_on_tuesday(mocker):
-    date = timezone.make_aware(datetime(2019, 10, 29))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_CPL1
+def test_should_return_launch_status_cpl1_on_mon_late(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 13, 23, 59, 59))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_CPL1
 
 
-def test_should_return_launch_status_cpl2_on_wednesday(mocker):
-    date = timezone.make_aware(datetime(2019, 10, 30))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_CPL2
+def test_should_return_launch_status_cpl2_on_tue_early(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 14))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_CPL2
 
 
-def test_should_return_launch_status_cpl2_on_thursday(mocker):
-    date = timezone.make_aware(datetime(2019, 10, 31))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_CPL2
+def test_should_return_launch_status_cpl2_on_tue_late(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 14, 23, 59, 59))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_CPL2
 
 
-def test_should_return_launch_status_cpl3_on_friday(mocker):
-    date = timezone.make_aware(datetime(2019, 11, 1))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_CPL3
+def test_should_return_launch_status_cpl3_on_wed_early(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 15))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_CPL3
 
 
-def test_should_return_launch_status_cpl3_on_saturday(mocker):
-    date = timezone.make_aware(datetime(2019, 11, 2))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_CPL3
+def test_should_return_launch_status_cpl3_on_wed_late(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 15, 23, 59, 59))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_CPL3
 
 
-def test_should_return_launch_status_cpl3_on_sunday(mocker):
-    date = timezone.make_aware(datetime(2019, 11, 3))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_CPL3
+def test_should_return_launch_status_cpl4_on_thu_early(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 16))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_CPL4
 
 
-def test_should_return_launch_status_open_cart_on_next_monday(mocker):
-    date = timezone.make_aware(datetime(2019, 11, 4))
-    mocker.patch('pythonpro.launch.views.timezone.now', return_value=date)
-    assert _get_launch_status() == LAUNCH_STATUS_OPEN_CART
+def test_should_return_launch_status_open_cart_after_last_class_start(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 16, 20))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_OPEN_CART
 
 
-@pytest.fixture
-def status_ppl(mocker):
-    return mocker.patch('pythonpro.launch.views._get_launch_status', return_value=0)
+def test_should_return_launch_status_open_cart_on_thu_late(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 16, 23, 59, 59))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_OPEN_CART
 
 
-def test_should_cpl1_redirect_to_landing_page(client, status_ppl):
-    response = client.get(reverse('launch:cpl1'), secure=True, redirect=False)
-    assert response.status_code == 302
+def test_should_return_launch_status_open_cart_on_last_day(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 21, 23, 59, 59))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_OPEN_CART
 
 
-def test_should_cpl2_redirect_to_landing_page(client, status_ppl):
-    response = client.get(reverse('launch:cpl2'), secure=True, redirect=False)
-    assert response.status_code == 302
+def test_should_return_launch_status_closed_on_day_after_closed(mocker):
+    fake_today = timezone.make_aware(datetime(2020, 4, 22))
+    mocker.patch('pythonpro.launch.facade.timezone.now', return_value=fake_today)
+    assert get_launch_status() == LAUNCH_STATUS_CLOSED
 
 
-def test_should_cpl3_redirect_to_landing_page(client, status_ppl):
-    response = client.get(reverse('launch:cpl3'), secure=True, redirect=False)
-    assert response.status_code == 302
+def test_should_return_only_cpl1(mocker):
+    mocker.patch('pythonpro.launch.facade.get_launch_status', return_value=LAUNCH_STATUS_CPL1)
+    assert get_opened_cpls() == ['cpl1']
 
 
-def test_should_not_redirect_to_landing_page_when_param_debug_was_setted(client, status_ppl):
-    response = client.get(reverse('launch:cpl1') + '?debug=1', secure=True, redirect=False)
-    assert response.status_code == 200
+def test_should_return_cpl1_to_2(mocker):
+    mocker.patch('pythonpro.launch.facade.get_launch_status', return_value=LAUNCH_STATUS_CPL2)
+    assert get_opened_cpls() == ['cpl1', 'cpl2']
 
 
-@pytest.fixture
-def status_cpl1(mocker):
-    return mocker.patch('pythonpro.launch.views._get_launch_status', return_value=1)
+def test_should_return_cpl1_to_3(mocker):
+    mocker.patch('pythonpro.launch.facade.get_launch_status', return_value=LAUNCH_STATUS_CPL3)
+    assert get_opened_cpls() == ['cpl1', 'cpl2', 'cpl3']
 
 
-@pytest.fixture
-def response_as_status_cpl1(mocker, status_cpl1, client):
-    return client.get(reverse('launch:cpl1'), secure=True)
-
-
-def test_should_show_video1_as_available_on_status_cpl1(response_as_status_cpl1):
-    dj_assert_contains(response_as_status_cpl1, 'video1-available.png')
-
-
-def test_should_show_video2_as_unavailable_on_status_cpl1(response_as_status_cpl1):
-    dj_assert_contains(response_as_status_cpl1, 'video2-unavailable.png')
-
-
-def test_should_show_video3_as_unavailable_on_status_cpl1(response_as_status_cpl1):
-    dj_assert_contains(response_as_status_cpl1, 'video3-unavailable.png')
-
-
-@pytest.fixture
-def status_cpl2(mocker):
-    return mocker.patch('pythonpro.launch.views._get_launch_status', return_value=2)
-
-
-@pytest.fixture
-def response_as_status_cpl2(mocker, status_cpl2, client):
-    return client.get(reverse('launch:cpl1'), secure=True)
-
-
-def test_should_show_video1_as_available_on_status_cpl2(response_as_status_cpl2):
-    dj_assert_contains(response_as_status_cpl2, 'video1-available.png')
-
-
-def test_should_show_video2_as_available_on_status_cpl2(response_as_status_cpl2):
-    dj_assert_contains(response_as_status_cpl2, 'video2-available.png')
-
-
-def test_should_show_video3_as_unavailable_on_status_cpl2(response_as_status_cpl2):
-    dj_assert_contains(response_as_status_cpl2, 'video3-unavailable.png')
-
-
-@pytest.fixture
-def status_cpl3(mocker):
-    return mocker.patch('pythonpro.launch.views._get_launch_status', return_value=3)
-
-
-@pytest.fixture
-def response_as_status_cpl3(mocker, status_cpl3, client):
-    return client.get(reverse('launch:cpl1'), secure=True)
-
-
-def test_should_show_video1_as_available_on_status_cpl3(response_as_status_cpl3):
-    dj_assert_contains(response_as_status_cpl3, 'video1-available.png')
-
-
-def test_should_show_video2_as_available_on_status_cpl3(response_as_status_cpl3):
-    dj_assert_contains(response_as_status_cpl3, 'video2-available.png')
-
-
-def test_should_show_video3_as_available_on_status_cpl3(response_as_status_cpl3):
-    dj_assert_contains(response_as_status_cpl3, 'video3-available.png')
+def test_should_return_cpl1_to_4(mocker):
+    mocker.patch('pythonpro.launch.facade.get_launch_status', return_value=LAUNCH_STATUS_CPL4)
+    assert get_opened_cpls() == ['cpl1', 'cpl2', 'cpl3', 'cpl4']
