146daafe917bca8f244bc3ecae51bee69fdc09c9
Moacir Moda <moa.moda@gmail.com>
2020-04-21 22:18:56 -0300

    allowing link redirectior

--- pythonpro/redirector/facade.py
+++ pythonpro/redirector/facade.py
@@ -1,5 +1,12 @@
+def _count_access_in_link(link):
+    link.total_access += 1
+    link.save()
+
+
 def get_redirect_url(redirect):
     if redirect.links.exists():
-        return redirect.links.order_by('?').first().url
+        link = redirect.links.order_by('total_access', 'id').first()
+        _count_access_in_link(link)
+        return link.url
 
     return redirect.url
--- pythonpro/redirector/models.py
+++ pythonpro/redirector/models.py
@@ -10,9 +10,13 @@ class Redirect(models.Model):
     url = models.URLField(null=True, blank=True)
     use_javascript = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.slug
+
 
 class RedirectLink(models.Model):
     created = models.DateTimeField("Criado em", default=timezone.now)
     updated = models.DateTimeField("Alterado em", auto_now=True)
     redirect = models.ForeignKey(Redirect, on_delete=models.PROTECT, related_name='links')
     url = models.URLField()
+    total_access = models.BigIntegerField(default=0)
