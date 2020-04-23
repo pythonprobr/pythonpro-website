291386f7127819d677088ea55ce53e55ad3bc571
Moacir Moda <moa.moda@gmail.com>
2020-04-23 10:53:36 -0300

    increasing redirector admin

--- pythonpro/redirector/admin.py
+++ pythonpro/redirector/admin.py
@@ -7,9 +7,11 @@ class RedirectLinkAdmin(admin.TabularInline):
     model = RedirectLink
     extra = 1
     readonly_fields = ['total_access', ]
+    exclude = ['created', 'updated']
 
 
 @admin.register(Redirect)
 class RedirectorAdmin(admin.ModelAdmin):
     list_display = 'slug url'.split()
     inlines = (RedirectLinkAdmin, )
+    exclude = ['created', 'updated']
