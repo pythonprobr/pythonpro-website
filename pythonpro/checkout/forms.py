31a06a4429925ac2044f95e1870cb74b73284f90
Moacir Moda <moa.moda@gmail.com>
2020-05-18 14:55:32 -0300

    making form to handle with errors

--- pythonpro/checkout/forms.py
+++ pythonpro/checkout/forms.py
@@ -2,6 +2,12 @@ from django import forms
 
 
 class WaitingForm(forms.Form):
-    first_name = forms.CharField()
-    email = forms.EmailField()
-    phone = forms.CharField()
+    first_name = forms.CharField(
+        label="", widget=forms.TextInput(attrs={'placeholder': 'Digite seu nome'})
+    )
+    email = forms.EmailField(
+        label="", widget=forms.EmailInput(attrs={'placeholder': 'Digite seu MELHOR email'})
+    )
+    phone = forms.CharField(
+        label="", widget=forms.TextInput(attrs={'placeholder': 'Digite seu WhatsApp (não se esqueça do DDD)'})
+    )
