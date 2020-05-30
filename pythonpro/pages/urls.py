4245076b6399452f068297f5a10c9f7c4f8b2dd8
Moacir Moda <moa.moda@gmail.com>
2020-05-30 12:25:22 -0300

    implementing data science webinar landing page. close #2430

--- pythonpro/pages/urls.py
+++ pythonpro/pages/urls.py
@@ -15,4 +15,14 @@ urlpatterns = [
         views.CarreiraProThankYouPage.as_view(),
         name='carreirapro_thank_you_page'
     ),
+    path(
+        'webinario-data-science/inscricao',
+        views.DsWebinarLandingPage.as_view(),
+        name='ds_webinar_landing_page'
+    ),
+    path(
+        'webinario-data-science/obrigado',
+        views.DsWebinarThankYouPage.as_view(),
+        name='ds_webinar_thank_you_page'
+    ),
 ]
