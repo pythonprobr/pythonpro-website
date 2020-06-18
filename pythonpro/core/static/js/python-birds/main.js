64a0a4e6b82cf9cdecc073caa217b15ff517bb77
Moacir Moda <moa.moda@gmail.com>
2020-06-18 10:23:47 -0300

    moving js scripts to page bottom

--- pythonpro/core/static/js/python-birds/main.js
+++ pythonpro/core/static/js/python-birds/main.js
@@ -1,10 +1,39 @@
 // SMOOTH SCROLL
-$('a[href^="#"]').on('click', function(event) {
+$('a[href^="#"]').on('click', function (event) {
   var target = $(this.getAttribute('href'));
   if (target.length) {
     event.preventDefault();
-    $('html, body').stop().animate( {
+    $('html, body').stop().animate({
       scrollTop: target.offset().top - 100
     }, 900);
   }
+});
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    autoPlay: 3000,
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      600: {
+        items: 3,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: true,
+      }
+    }
+  })
+});
+
+$('.popup-vimeo').magnificPopup({
+  src: 'https://vimeo.com/398972182',
+  type: 'iframe'
 });
