597f5ac5891fcb27129625b6911d0d0a38f04272
renzon <renzon@gmail.com>
2020-07-08 13:56:00 -0300

    Removed Pytools cli and user creation on admin
    related to 2411

--- pythonpro/payments/tests/test_pytools_promotion_cli.py
+++
@@ -1,8 +0,0 @@
-from django.core.management import call_command
-
-
-def test_pytools_command(mocker):
-    mock = mocker.patch(
-        'pythonpro.payments.management.commands.pytools_promotion.user_facade.run_pytools_promotion_campaign')
-    call_command('pytools_promotion')
-    mock.assert_called_once_with()
