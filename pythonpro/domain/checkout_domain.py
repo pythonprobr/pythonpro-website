768451c5accb026af30ce6756f6cff887641c9bb
renzon <renzon@gmail.com>
2020-10-04 09:25:09 -0300

    Promoted webinars buyers to webdev role
    close #3030

--- pythonpro/domain/checkout_domain.py
+++ pythonpro/domain/checkout_domain.py
@@ -80,7 +80,7 @@ def _promote(user, slug: str):
         user_domain.promote_member(user, 'unknown')
     elif slug == 'pacote-proximo-nivel-67-discount':
         user_domain.promote_pythonista(user, 'unknown')
-    elif slug.startswith('webdev'):
+    elif slug.startswith('webdev') or slug == 'treinamento-devpro-webinar':
         user_domain.promote_webdev(user, 'unknown')
     elif slug.startswith('data-science'):
         user_domain.promote_data_scientist(user, 'unknown')
