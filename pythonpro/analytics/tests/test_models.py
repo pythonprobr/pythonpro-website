4810dc8ae07684bf0016b8f137815957a20b5ce4
Ramiro Alvaro <ramiroalvaro@hotmail.com>
2020-10-07 15:24:13 -0300

    Changed import JSONField from django.contrib.postgres.fields to django.db.models
    close #2825

--- pythonpro/analytics/tests/test_models.py
+++ pythonpro/analytics/tests/test_models.py
@@ -10,4 +10,4 @@ def test_should_exists_session_model():
 
 @pytest.mark.django_db
 def test_should_exists_page_view_model():
-    assert baker.make('PageView')
+    assert baker.make('PageView', meta={'name': 'Renzo'})
