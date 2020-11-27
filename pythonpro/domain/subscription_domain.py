6d4f1838d0b841a2726867ffcd2384cfe87ca1e0
renzon <renzon@gmail.com>
2020-11-27 12:59:01 -0300

    Client registration on email marketing before analytics
    close #3235

--- pythonpro/domain/subscription_domain.py
+++ pythonpro/domain/subscription_domain.py
@@ -10,5 +10,8 @@ run_until_available = shared_task(autoretry_for=(JSONDecodeError,), retry_backof
 
 @run_until_available
 def subscribe_with_no_role(session_id, name: str, email: str, *tags, id='0', phone=None):
+    result = create_or_update_with_no_role(name, email, *tags, id=id, phone=phone)
+    # order matter, better register client on email marketing before
+    # posthog registration
     posthog_alias(session_id, email)
-    return create_or_update_with_no_role(name, email, *tags, id=id, phone=phone)
+    return result
