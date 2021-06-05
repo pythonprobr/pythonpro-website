989c47a2a694069cd0a3ac3ecc85dd16634eb609
renzon <renzon@gmail.com>
2021-06-05 13:10:39 -0300

    Implemented relationship between Pagarme Item and Subscription Type
    close #3761

--- pythonpro/memberkit/models.py
+++ pythonpro/memberkit/models.py
@@ -11,3 +11,19 @@ class SubscriptionType(models.Model):
     class Meta:
         verbose_name = 'Tipo de Assinatura'
         verbose_name_plural = 'Tipos de Assinaturas'
+
+
+class PaymentItemConfigToSubscriptionType(models.Model):
+    payment_item = models.OneToOneField('django_pagarme.PagarmeItemConfig', null=False, on_delete=models.CASCADE,
+                                        related_name='subscription_type_relation')
+    subscription_type = models.ForeignKey(SubscriptionType, null=False, on_delete=models.CASCADE,
+                                          related_name='payment_items_relation')
+
+    def __str__(self):
+        return f'{self.payment_item} -> {self.subscription_type}'
+
+# class Subscription(models.Model):
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now_add=True)
+#     payment = models.OneToOneField('PagarmePayment', on_delete=models.DO_NOTHING, )
+#     subscription_type = models.OneToOneField(SubscriptionType, null=False, on_delete=models.DO_NOTHING)
