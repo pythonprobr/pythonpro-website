63515aa81849d03d2002d71fac259682eced1ba4
Moacir Moda <moa.moda@gmail.com>
2021-10-11 18:51:43 -0300

    changing thanks page of Python Birds do advertise L8

--- pythonpro/core/views.py
+++ pythonpro/core/views.py
@@ -184,7 +184,9 @@ def _lead_form(request, *args, **kwargs):
 
     login(request, user)
 
-    return redirect(reverse('core:thanks'))
+    # return redirect(reverse('core:thanks'))
+    return redirect('https://pythonpro.com.br/jornada-rumo-a-primeira-vaga-inscricao-l8-v4-iscas/'
+                    '?utm_source=iscas&utm_medium=trafego-organico&utm_campaign=L8')
 
 
 def lead_form(request):
