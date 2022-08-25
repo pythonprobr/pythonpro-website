5b8b675b787467c1d2906886d30be6b8237aa0c6
renzon <renzon@gmail.com>
2022-08-25 00:33:43 -0300

    Created tests for Discord API client
    part of #4711

--- pythonpro/discord/api_client.py
+++ pythonpro/discord/api_client.py
@@ -1,4 +1,5 @@
 from functools import cached_property
+from typing import Union
 
 import requests
 from requests import HTTPError
@@ -15,9 +16,8 @@ class UnathorizedOperation(Exception):
     pass
 
 
-class DiscordClient:
-    def __init__(self, access_token, bot_token=None):
-        self._bot_token = bot_token
+class DiscordAppClient:
+    def __init__(self, access_token: str):
         self._access_token = access_token
 
     @cached_property
@@ -30,6 +30,58 @@ class DiscordClient:
         discord_user_dict = r.json()
         return discord_user_dict
 
+
+class DiscordBotClient:
+    def __init__(self, bot_token: str):
+        self._bot_token = bot_token
+
+    def list_guild_members(self, guild_id, limit=1, after=0) -> dict:
+        """
+        :param guild_id: the discord server id
+        """
+        headers = {
+            'Authorization': f'Bot {self._bot_token}'
+        }
+        data = {'limit': limit, 'after': after}
+        r = requests.get(
+            f'{_BASE_ENDPOINT_URI}/guilds/{guild_id}/members',
+            # data=data,
+            headers=headers)
+        try:
+            r.raise_for_status()
+        except HTTPError as error:
+            if error.response.status_code == 403:
+                raise UnathorizedOperation(
+                    f"You have no permission to list member of Guild with id {guild_id}"
+                    f"\nCheck if Guild id is right and you have added your Discord bot on Guild"
+                ) from error
+        else:
+            return r.json()
+
+    def remove_guild_member(self, guild_id, user_id):
+        headers = {
+            'Authorization': f'Bot {self._bot_token}'
+        }
+        r = requests.delete(
+            f'{_BASE_ENDPOINT_URI}/guilds/{guild_id}/members/{user_id}',
+            headers=headers)
+        try:
+            r.raise_for_status()
+        except HTTPError as error:
+            if error.response.status_code == 403:
+                raise UnathorizedOperation(
+                    f"You have no permission to remove member {user_id} of Guild with id {guild_id}"
+                    f"\nCheck if Guild id is right and you have added your Discord bot on Guild"
+                    "\nAlso check that Bot has role bigger than the member you want to kick. Check more info on:"
+                    "\nhttps://discord.com/developers/docs/topics/permissions#permission-hierarchy"
+                ) from error
+
+
+class DiscordAppAndBotClient(DiscordAppClient, DiscordBotClient):
+    def __init__(self, access_token: str, bot_token: str):
+        DiscordAppClient.__init__(self, access_token)
+        DiscordBotClient.__init__(self, bot_token)
+
     def add_guild_member(self, guild_id) -> dict:
         """
         :param guild_id: the discord server id
@@ -37,8 +89,6 @@ class DiscordClient:
 
         If user is already on guild, return a member dict but only with user data.
         """
-        if self._bot_token is None:
-            raise MissingBotToken()
         current_user = self.current_user
         headers = {
             'Authorization': f'Bot {self._bot_token}'
@@ -70,7 +120,7 @@ class DiscordCredentials:
         self._app_client_secret = app_client_secret
         self._redirect_uri = redirect_uri
 
-    def generate_api_client(self, autorization_code: str) -> DiscordClient:
+    def generate_api_client(self, autorization_code: str) -> Union[DiscordAppClient, DiscordAppAndBotClient]:
         data = {
             'client_id': self._app_client_id,
             'client_secret': self._app_client_secret,
@@ -85,4 +135,12 @@ class DiscordCredentials:
         r.raise_for_status()
         oauth2_data = r.json()
         access_token = oauth2_data['access_token']
-        return DiscordClient(access_token, self._bot_token)
+        if self._bot_token is None:
+            return DiscordAppClient(access_token)
+        return DiscordAppAndBotClient(access_token, self._bot_token)
+
+
+if __name__ == '__main__':
+    c = DiscordBotClient('MTAwNDU0MTg3NTk1NDQwNTQzNg.GFcT5X.Kp4gVn0U1kOPvdwLku-oki7LI_wtbMma2E_ET4')
+    members = c.list_guild_members(971162582624903288)
+    print(members)
