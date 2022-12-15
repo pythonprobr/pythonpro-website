572713bc6b2ad4565730fbca5f2ef59fb58b9fa2
renzon <renzon@gmail.com>
2022-12-15 12:57:43 -0300

    Removed cohort links
    part of #4776

--- pythonpro/cohorts/views.py
+++ pythonpro/cohorts/views.py
@@ -7,11 +7,6 @@ from pythonpro.memberkit import facade as memberkit_facade
 
 
 @login_required
-def detail(request, slug):
-    return render(request, 'cohorts/cohort_detail.html', {'cohort': facade.find_cohort(slug=slug)})
-
-
-@login_required
 def webinars(request):
     return render(request, 'cohorts/webinars.html', {'webinars': facade.find_recorded_webinars()})
 
