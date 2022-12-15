992a29fb1c773d4b51f4123759c97f951a2979f0
renzon <renzon@gmail.com>
2022-12-15 13:23:11 -0300

    Removed cohort webinário link and added Discord Link
    close #4776

--- pythonpro/cohorts/urls.py
+++ pythonpro/cohorts/urls.py
@@ -4,7 +4,5 @@ from pythonpro.cohorts import views
 
 app_name = 'cohorts'
 urlpatterns = [
-    path('webinarios/', views.webinars, name='webinars'),
-    path('webinarios/<slug:slug>/', views.webinar, name='webinar'),
     path('aulas/<int:pk>/', views.live_class, name='live_class'),
 ]
--- pythonpro/cohorts/views.py
+++ pythonpro/cohorts/views.py
@@ -1,25 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect, render
+from django.shortcuts import redirect
 from django.urls import reverse
 
 from pythonpro.cohorts import facade
 from pythonpro.memberkit import facade as memberkit_facade
-
-
-@login_required
-def webinars(request):
-    return render(request, 'cohorts/webinars.html', {'webinars': facade.find_recorded_webinars()})
-
-
-@login_required
-def webinar(request, slug):
-    user = request.user
-    if memberkit_facade.has_memberkit_account(user):
-        webinar = facade.find_webinar(slug=slug)
-        return redirect(webinar.memberkit_url, permanent=True)
-    if memberkit_facade.has_any_subscription(user):
-        return redirect(reverse('migrate_to_memberkit'), permanent=True)
-    return redirect(reverse('checkout:bootcamp_lp'), permanent=False)
 
 
 @login_required
