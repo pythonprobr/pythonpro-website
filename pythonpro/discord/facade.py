7e278fa4e5c586feb416ae357e93c9b0347dd4e8
renzo <renzon@gmail.com>
2024-05-23 18:32:06 -0300

    Implemented warning sales msgs on channel
    Close #4870

--- pythonpro/discord/facade.py
+++ pythonpro/discord/facade.py
@@ -1,9 +1,15 @@
 import logging
+from datetime import timedelta
 
 from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.db.models import Max
 
-from pythonpro.discord.bot import discord_bot_client
-from pythonpro.discord.tasks import clean_discord_user
+from pythonpro.discord.bot import devpro_discord_bot_client
+from pythonpro.discord.tasks import clean_discord_user, warn_subscription_expiration
+from pythonpro.memberkit.models import Subscription
+
+from django.utils.timezone import datetime
 
 logger = logging.getLogger(__name__)
 
@@ -11,7 +17,7 @@ logger = logging.getLogger(__name__)
 def clean_discord_users():
     discord_user_id = 0
     while True:
-        discord_members = discord_bot_client.list_guild_members(settings.DISCORD_GUILD_ID, after=discord_user_id)
+        discord_members = devpro_discord_bot_client.list_guild_members(settings.DISCORD_GUILD_ID, after=discord_user_id)
         if len(discord_members) == 0:
             break
         for member in discord_members:
@@ -25,4 +31,16 @@ def clean_discord_users():
 
 
 def warn_users_about_subscription_expiration():
-    return None
+    today = datetime.today()
+    thirty_days_in_future = today + timedelta(days=30)
+    users_with_subscription_expirating = get_user_model().objects.annotate(
+        max_subscription_expiration_date=Max('subscriptions__expired_at')
+    ).filter(
+        subscriptions__status=Subscription.Status.ACTIVE,
+        max_subscription_expiration_date__lte=thirty_days_in_future
+    ).values('id', 'max_subscription_expiration_date')
+    for user_dct in users_with_subscription_expirating:
+        warn_subscription_expiration.delay(
+            user_dct['id'],
+            user_dct['max_subscription_expiration_date'].strftime('%d/%m/%Y')
+        )
--- pythonpro/discord/tests/test_commands.py
+++ pythonpro/discord/tests/test_commands.py
@@ -1,10 +1,51 @@
+from datetime import timedelta
+
 from django.core import management
+from django.utils.datetime_safe import datetime
+from model_bakery import baker
+
+from pythonpro.memberkit.models import Subscription
 
 
 def test_clean_discord_users_command(mocker):
-    mocker.patch('pythonpro.discord.facade.discord_bot_client.list_guild_members', return_value=[])
+    mocker.patch('pythonpro.discord.facade.devpro_discord_bot_client.list_guild_members', return_value=[])
     management.call_command('clean_discord_users')
 
 
-def test_warn_users_about_subscriptions():
+def test_warn_users_about_subscriptions(mocker, django_user_model):
+    mock = mocker.patch('pythonpro.discord.facade.warn_subscription_expiration', return_value=[])
+    subscriber = baker.make(django_user_model)
+    expiration_date = datetime.today() + timedelta(days=29)
+    baker.make(
+        Subscription, status=Subscription.Status.ACTIVE,
+        expired_at=expiration_date,
+        subscriber=subscriber
+    )
     management.call_command('warn_users_about_subscription_expiration')
+    mock.delay.assert_called_once_with(subscriber.id, expiration_date.strftime('%d/%m/%Y'))
+
+
+def test_dont_botter_user_with_inactive_subscriptions(mocker, django_user_model):
+    mock = mocker.patch('pythonpro.discord.facade.warn_subscription_expiration', return_value=[])
+    subscriber = baker.make(django_user_model)
+    expiration_date = datetime.today() + timedelta(days=29)
+    baker.make(
+        Subscription, status=Subscription.Status.INACTIVE,
+        expired_at=expiration_date,
+        subscriber=subscriber
+    )
+    management.call_command('warn_users_about_subscription_expiration')
+    assert not mock.delay.called
+
+
+def test_dont_botter_user_with_more_than_30_days_subscription(mocker, django_user_model):
+    mock = mocker.patch('pythonpro.discord.facade.warn_subscription_expiration', return_value=[])
+    subscriber = baker.make(django_user_model)
+    expiration_date = datetime.today() + timedelta(days=31)
+    baker.make(
+        Subscription, status=Subscription.Status.ACTIVE,
+        expired_at=expiration_date,
+        subscriber=subscriber
+    )
+    management.call_command('warn_users_about_subscription_expiration')
+    assert not mock.delay.called
